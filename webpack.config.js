90d6e6ba8974e065de904bf1d246baacafc90c15
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2026-01-30 12:17:36 +0000

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -11,12 +13,15 @@ module.exports = (env, argv) => {
     output: {
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       path: path.resolve(__dirname, 'docs'),
-      assetModuleFilename: 'assets/[name].[hash][ext][query]'
+      assetModuleFilename: 'assets/[name].[hash][ext][query]',
+      clean: true
     },
     resolve: {
+      extensions: ['.ts', '.js', '.json'],
       alias: {
         '@fonts': path.resolve(__dirname, 'src/styles/@fonts'),
         '@images': path.resolve(__dirname, 'src/images'),
+        '@': path.resolve(__dirname, 'src')
       }
     },
     devServer: {
@@ -26,10 +31,43 @@ module.exports = (env, argv) => {
       compress: true,
       port: 9000,
       hot: true,
-      open: true
+      open: true,
+      liveReload: true,
+      historyApiFallback: true
     },
     module: {
       rules: [
+        // TypeScript
+        {
+          test: /\.tsx?$/i,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true
+              }
+            }
+          ]
+        },
+        // Babel
+        {
+          test: /\.(js|ts)$/i,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                  '@babel/preset-typescript'
+                ],
+                cacheDirectory: true
+              }
+            }
+          ]
+        },
+        // CSS
         {
           test: /\.css$/i,
           use: [
@@ -37,13 +75,33 @@ module.exports = (env, argv) => {
             'css-loader'
           ],
         },
+        // SCSS/Sass
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          test: /\.scss$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader'
+          ],
+        },
+        // Less
+        {
+          test: /\.less$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'less-loader'
+          ],
+        },
+        // Изображения
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
           type: 'asset/resource',
           generator: {
             filename: 'images/[name].[hash][ext]'
           }
         },
+        // Шрифты
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/i,
           type: 'asset/resource',
@@ -55,13 +113,19 @@ module.exports = (env, argv) => {
     },
     plugins: [
       new CleanWebpackPlugin(),
+      new ESLintPlugin({
+        extensions: ['js', 'ts'],
+        fix: true
+      }),
       new HtmlWebpackPlugin({
         title: 'Wuthering Waves Characters',
         template: './src/index.html',
+        favicon: './src/images/logo.png'
       }),
       new MiniCssExtractPlugin({
         filename: isProduction ? '[name].[contenthash].css' : '[name].css',
       }),
+      ...(argv.analyze ? [new BundleAnalyzerPlugin()] : [])
     ],
     optimization: {
       splitChunks: {
@@ -75,5 +139,6 @@ module.exports = (env, argv) => {
       },
       runtimeChunk: 'single',
     },
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
   };
 };
